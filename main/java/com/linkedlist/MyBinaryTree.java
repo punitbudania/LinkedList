e1dbbabee9eacde44fcd447a9a22a32fa2620ba1
punitbudania <punitbudania88@gmail.com>
2020-12-07 20:00:02 +0530

    [punit]Added: search for an element UC 3

--- main/java/com/linkedlist/MyBinaryTree.java
+++ main/java/com/linkedlist/MyBinaryTree.java
@@ -40,4 +40,28 @@ public class MyBinaryTree<K extends Comparable<K>>
         return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
                                         + this.getSizeRecursive(current.right);
     }
+
+    public boolean search(K key)
+    {
+        return this.searchRecursively(root, key);
+    }
+
+    private boolean searchRecursively(MyBinaryNode<K> current, K key)
+    {
+        if (current == null)
+        {
+            return false;
+        }
+        int compareResult = key.compareTo(current.key);
+        if (compareResult == 0) return true;
+        if (compareResult < 0)
+        {
+            current.left = addRecursively(current.left, key);
+        }
+        else
+        {
+            current.right = addRecursively(current.right, key);
+        }
+        return true;
+    }
 }
--- test/java/com/testcases/MyBinaryTreeTest.java
+++ test/java/com/testcases/MyBinaryTreeTest.java
@@ -38,4 +38,26 @@ public class MyBinaryTreeTest
         int size = myBinaryTree.getSize();
         Assert.assertEquals(13, size);
     }
+
+    @Test
+    public void searchForElement_IfPresent_ReturnTrue()
+    {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(40);
+        myBinaryTree.add(22);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+
+        boolean check = myBinaryTree.search(63);
+        Assert.assertTrue(check);
+    }
 }
